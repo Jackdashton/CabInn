5e1464dcc7ef3b4bcbf2c624ca6d9a5a3c2cef2a
jack ashton <jackdashton@outlook.com>
2023-03-31 00:13:11 +0200

    added username to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "pages#home"
+
+  get "/about", to: "pages#about"
+  get "/home", to: "pages#home"
 end
