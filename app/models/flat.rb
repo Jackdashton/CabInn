e93e0c8fecdf808f607f31e6740f6f852c9eec15
jack ashton <jackdashton@outlook.com>
2023-04-01 15:05:56 +0200

    added flats controller

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,5 @@
 class Flat < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-
   get "/about", to: "pages#about"
   get "/home", to: "pages#home"
+
+  get "/flats", to: "flats#index"
+  get "/flats/:id", to: "flats#show"
+  get "/flats/new", to: "flats#new"
 end
