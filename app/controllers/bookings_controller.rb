6462919aa95e2eaebfc59d498da30783ac74fc81
jack ashton <jackdashton@outlook.com>
2023-04-06 20:01:56 +0200

    added bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,35 @@
 class BookingsController < ApplicationController
+  before_action :set_flat, only: [:new, :create, :update]
+  before_action :set_booking, only: [:update, :destroy]
+
   def new
     @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new
+    @booking.flat = @flat
+    # Need reference to current flat to be used in the view page.
+    @user = current_user
+    # booking status is false
+    @booking.status = false
+    if @booking.save!
+      redirect_to bookings_path
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flat_id)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
