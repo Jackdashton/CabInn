5d4b5fc94a4284327cefb3358865e47666a66c9a
jack ashton <jackdashton@outlook.com>
2023-04-07 15:33:18 +0200

    fixed bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,40 @@
 class BookingsController < ApplicationController
-  before_action :set_flat, only: [:new, :create, :update]
-  before_action :set_booking, only: [:update, :destroy]
+  before_action :set_flat, only: %i[new create update]
+  before_action :set_booking, only: %i[update destroy]
+
+  # We need to create a booking and render a form(new)
+  # We need to update the status - accepted or not (true/false)
+  # We need to be able to delete a booking.
+  # new create and update need to have a flat id, delete does not. 
 
   def new
-    @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
   end
 
   def create
+    # For instannce of booking, we need an instance of user and flat.
     @booking = Booking.new
     @booking.flat = @flat
-    # Need reference to current flat to be used in the view page.
-    @user = current_user
-    # booking status is false
+    @booking.user = current_user
+    # Booking status false when initialised.
     @booking.status = false
+    # if the booking can be saved with validations, then redirect otherwise re-render form.
     if @booking.save!
-      redirect_to bookings_path
+      redirect_to flats_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @booking.status = true
+    @booking.save
+    redirect_to flats_path
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to flats_path
   end
 
   private
