17d05f7ae0a5a964f697a9d700fb985d1e8e5c47
jack ashton <jackdashton@outlook.com>
2023-04-07 16:59:59 +0200

    Updated navbar with logo, avatar and dropdown options

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,11 @@ class PagesController < ApplicationController
   def bookings
     @bookings = Booking.all
     @user = current_user
+    # request bookings belong to specific user and are not tbc (false)
     @requested_bookings = @bookings.select do |booking|
       booking.user == @user && booking.status == false
     end
+    # reserved bookings are true
     @reserved_bookings = @user.bookings.where(status: true)
     @flats = @user.flats
   end
