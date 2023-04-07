fab48a7e4961551ea1194f55ae8d462bd4c95213
jack ashton <jackdashton@outlook.com>
2023-04-07 16:17:13 +0200

    bookings page - booking id added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,12 @@ class PagesController < ApplicationController
   end
 
   def bookings
+    @bookings = Booking.all
+    @user = current_user
+    @requested_bookings = @bookings.select do |booking|
+      booking.user == @user && booking.status == false
+    end
+    @reserved_bookings = @user.bookings.where(status: true)
+    @flats = @user.flats
   end
 end
