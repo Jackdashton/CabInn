428b4084ca6b9346c2cd97a190665b3e7a074509
jack ashton <jackdashton@outlook.com>
2023-04-13 13:03:23 +0100

    Added marker to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,14 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static values = { apiKey: String }
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
     // Must hide API key
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.element, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      zoom: 9, // starting zoom
+      zoom: 7, // starting zoom
     });
+    // Private function
+    this.#addMarkersToMap()
   }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+    // Create a new marker, set the longitude and latitude, and add it to the map.
+    new mapboxgl.Marker()
+    .setLngLat([marker.lng, marker.lat])
+    .addTo(this.map);
+    // map is only in the connect function, so must be this.map to be instance variable
+  })
+}
 }
