4789d5e0a5271ea9c5c1d88f6dd0bb40d6bad34c
jack ashton <jackdashton@outlook.com>
2023-04-13 13:28:31 +0100

    centered the markers over flat

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,10 +13,10 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      zoom: 7, // starting zoom
     });
     // Private function
     this.#addMarkersToMap()
+    this.#fitMapToMarkers()
   }
 
   #addMarkersToMap() {
@@ -27,5 +27,12 @@ export default class extends Controller {
     .addTo(this.map);
     // map is only in the connect function, so must be this.map to be instance variable
   })
-}
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 11, duration: 0 })
+  }
+
 }
