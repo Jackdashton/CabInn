d6cb74806b6bf959187d75b822666e77bcb5ba55
jack ashton <jackdashton@outlook.com>
2023-05-07 16:08:07 +0100

    added flat tests

--- test/models/flat_test.rb
+++ test/models/flat_test.rb
@@ -1,12 +1,31 @@
 require "test_helper"
 
 class FlatTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
-
-  test "should not save flat without title" do
+  test "should not save flat without name" do
     flat = Flat.new
     assert_not flat.save
   end
+
+  test "has a description" do
+    flat = Flat.new(
+      name: "Cabin",
+      address: "30 Drachenseestrasse",
+      description: "",
+      guest_num: 5,
+      price_per_night: 45
+    )
+    assert_not flat.save
+  end
+
+  test "has a price per night between 1 and 20000 euros" do
+    flat = Flat.new(
+      name: "Cabin",
+      address: "30 Drachenseestrasse",
+      description: "Amazing",
+      guest_num: 5,
+      price_per_night: 45
+    )
+    assert_not flat.save
+  end
+
 end
